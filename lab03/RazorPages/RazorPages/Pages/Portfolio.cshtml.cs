11bb06341bc09deca0905f5bd121ffeb0032e755
NikitaBogoslovskiy <nbog@sfedu.ru>
2024-03-23 21:00:24 +0300

    do lab05

--- lab03/RazorPages/RazorPages/Pages/Portfolio.cshtml.cs
+++ lab03/RazorPages/RazorPages/Pages/Portfolio.cshtml.cs
@@ -1,10 +1,71 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json;
+using Newtonsoft.Json.Linq;
 
 namespace RazorPages.Pages
 {
+/*    public class Banner
+    {
+        public string Title { get; set; }
+        public string MainPage { get; set; }
+        public string ThisPage { get; set; }
+    }
+
+    public class Header
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class ProjectCard
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+    public class PortfolioContent
+    {
+        public Banner Banner { get; set; }
+        public Header Header { get; set; }
+        public List<string> Filters { get; set; }
+        public List<ProjectCard> ProjectCards { get; set; }
+    }*/
+
+    public interface IContentService
+    {
+        public void Load(string path);
+        public string? Get(string tokenPath);
+    }
+
+    public class ContentService : IContentService
+    {
+        private JObject content;
+
+        public void Load(string path)
+        {
+            content = JObject.Parse(File.ReadAllText(path));
+        }
+
+        public string? Get(string tokenPath)
+        {
+            return (string?)content.SelectToken(tokenPath);
+        }
+    }
+
     public class PortfolioModel : PageModel
     {
+        public readonly IContentService contentService;
+        private readonly string pathToContent = "wwwroot/PortfolioContent.json";
+
+        public PortfolioModel(IContentService _contentService)
+        {
+            contentService = _contentService;
+            contentService.Load(pathToContent);
+        }
+
         public void OnGet()
         {
         }
--- lab03/RazorPages/RazorPages/Program.cs
+++ lab03/RazorPages/RazorPages/Program.cs
@@ -1,7 +1,10 @@
+using RazorPages.Pages;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IContentService, ContentService>();
 
 var app = builder.Build();
 

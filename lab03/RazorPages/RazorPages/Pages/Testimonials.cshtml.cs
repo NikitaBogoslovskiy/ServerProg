de62dd652cb2d50ece747b61b529d6c68a1c9e77
NikitaBogoslovskiy <nbog@sfedu.ru>
2024-04-15 00:05:11 +0300

    add testimonial form

--- lab03/RazorPages/RazorPages/Pages/Testimonials.cshtml.cs
+++ lab03/RazorPages/RazorPages/Pages/Testimonials.cshtml.cs
@@ -3,18 +3,79 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPages.Data;
 using RazorPages.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Drawing;
 
 namespace RazorPages.Pages
 {
+    [Bind]
+    public class TestimonialForm
+    {
+        [BindProperty]
+        [Required]
+        public string? Name { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string? Occupation { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string? Title { get; set; }
+
+        [BindProperty]
+        [Required]
+        public string? Description { get; set; }
+
+        [BindProperty]
+        public IFormFile? Image { get; set; }
+    }
+
     public class TestimonialsModel : PageModel
     {
         private readonly RazorPagesContext context;
+        public TestimonialForm InputForm { get; set; } = default!;
         public List<Testimonial> Testimonials { get; set; } = new();
+        private readonly string rootPath = "wwwroot";
+        private readonly string uploadsPath = "uploads";
 
         public TestimonialsModel(RazorPagesContext context)
         {
             this.context = context;
             Testimonials = this.context.Testimonials.ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+                return Page();
+
+            var pathToImage = Path.Combine(uploadsPath, "default_image.png");
+            if (InputForm.Image != null)
+            {
+                var fileName = Guid.NewGuid().ToString() + ".png";
+                var relativePath = Path.Combine(uploadsPath, fileName);
+                var fullPath = Path.Combine(rootPath, relativePath);
+                using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await InputForm.Image.CopyToAsync(fileStream);
+                }
+                Utils.ClipToCircle(fullPath);
+                pathToImage = relativePath;
+            }
+
+            context.Testimonials.Add(new Testimonial
+            {
+                Id = 0,
+                Name = InputForm.Name,
+                Occupation = InputForm.Occupation,
+                Title = InputForm.Title,
+                Description = InputForm.Description,
+                ImgUrl = pathToImage
+            });
+            await context.SaveChangesAsync();
+            return RedirectToPage("/Testimonials");
         }
     }
 }
---
+++ lab03/RazorPages/RazorPages/Utils.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing.Drawing2D;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.CompilerServices;
+
+namespace RazorPages
+{
+    public static class Utils
+    {
+        #pragma warning disable CA1416
+
+        public static bool ClipToCircle(string imagePath)
+        {
+            var srcImage = Image.FromFile(imagePath);
+            var srcCenterX = srcImage.Width / 2f;
+            var srcCenterY = srcImage.Height / 2f;
+            var radius = Math.Min(srcCenterX, srcCenterY);
+            var sideLength = (int)radius * 2;
+            Image dstImage = new Bitmap(sideLength, sideLength, PixelFormat.Format32bppArgb);
+
+            using (Graphics g = Graphics.FromImage(dstImage))
+            {
+                RectangleF r = new RectangleF(0, 0, sideLength, sideLength);
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                using (Brush br = new SolidBrush(Color.Transparent))
+                {
+                    g.FillRectangle(br, 0, 0, dstImage.Width, dstImage.Height);
+                }
+
+                GraphicsPath path = new GraphicsPath();
+                path.AddEllipse(r);
+                g.SetClip(path);
+                g.DrawImage(srcImage, -(srcImage.Width - sideLength) / 2f, -(srcImage.Height - sideLength) / 2f);
+            }
+
+            srcImage.Dispose();
+            File.Delete(imagePath);
+            dstImage.Save(imagePath);
+            return true;
+        }
+    }
+}

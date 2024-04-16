5877e6baa1e70a6330872902d4f4e84a1e316e5c
NikitaBogoslovskiy <nbog@sfedu.ru>
2024-04-16 21:37:23 +0300

    add rest api for movies

---
+++ lab07/RazorPages/RazorPages/Data/MoviesContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPages.Data.Configurations;
+using RazorPages.Models;
+
+namespace RazorPages.Data
+{
+    public class MoviesContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source=movies.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new MovieConfiguration());
+        }
+    }
+}

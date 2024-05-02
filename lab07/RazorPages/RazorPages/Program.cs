5baee39b58b78a5841fa848acecdd96df27bf2a4
NikitaBogoslovskiy <nbog@sfedu.ru>
2024-05-02 03:04:06 +0300

    add jwt auth

--- lab07/RazorPages/RazorPages/Program.cs
+++ lab07/RazorPages/RazorPages/Program.cs
@@ -1,10 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.IdentityModel.Tokens;
+using RazorPages.Controllers;
 using RazorPages.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+        };
+    });
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MoviesContext>();
+builder.Services.AddSession();
 
 var app = builder.Build();
 
@@ -14,6 +33,17 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseSession();
+app.Use(async (context, next) =>
+{
+    var token = context.Session.GetString("Token");
+    if (!string.IsNullOrEmpty(token))
+    {
+        context.Request.Headers.Add("Authorization", "Bearer " + token);
+    }
+    await next();
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
